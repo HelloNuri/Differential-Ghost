cf5130c8de515919089321d77dce54f15dfb5626
HelloNuri <HelloNuri.dvl@gmail.com>
2018-06-08 09:34:34 +0900

    difference

---
+++ Assets/Script/GameDirector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
